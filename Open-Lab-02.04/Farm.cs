a72cce39f3945221d08320475023213c8072a3f9
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-09-29 22:11:56 +0200

    Implemented the method that returns a number of legs.

--- Open-Lab-02.04/Farm.cs
+++ Open-Lab-02.04/Farm.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_02._04
     {
         public int GetLegsCount(int chickens, int cows, int pigs)
         {
-            throw new NotImplementedException();
+            var kury = 2 * chickens;
+            var kravy = 4 * cows;
+            var prasa = 4 * pigs;
+            return kury + kravy + prasa; 
         }
     }
 }
